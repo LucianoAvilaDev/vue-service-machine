1a9af98fc5f2b5cbbc03ed5e718ace1e3ffa1e0e
LucianoAvilaDev <luciano.avila.dev@outlook.com>
2023-04-24 10:36:14 -0300

    bill implementation

--- scripts.js
+++ scripts.js
@@ -69,6 +69,19 @@ const SelfServiceMachine = {
         return {
             products: window.products
         }
+    },
+    methods: {
+        total: function(){
+            var total = 0
+
+            this.products.forEach((item) => {
+                if(item.active){
+                    total += item.price * item.quantity
+                }
+            })
+
+            return total.toFixed(2)
+        }
     }
 }
 
